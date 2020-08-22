9d0f2c70d5fbdc70da09bf8ee505524d4b8439f2
Neelam22Patel <neelam.uvbm@gmail.com>
2020-08-21 23:47:28 -0400

    added 2 more condition

---
+++ p1/src/p2.java
@@ -0,0 +1,14 @@
+
+public class p2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String hungry="yes";
+		if(hungry=="yes"|| hungry=="Y" || hungry=="y") {
+			System.out.println("eat something");
+		}else {
+			System.out.println("go work");
+		}
+	}
+
+}
